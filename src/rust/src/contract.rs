a081e8fa676d9ef311b7dfe502c4ec1dd0df55f9
Graydon Hoare <graydon@pobox.com>
2022-06-07 17:09:30 -0700

    Factor and comment contract.rs

--- src/rust/src/contract.rs
+++ src/rust/src/contract.rs
@@ -10,51 +10,67 @@ use cxx::CxxString;
 use im_rc::OrdMap;
 use std::error::Error;
 use stellar_contract_env_host::{
-    storage, xdr,
+    storage::{self, AccessType, Key, Storage},
+    xdr,
     xdr::{
-        ContractDataEntry, LedgerEntry, LedgerEntryData, ReadXdr, ScObject, ScStatic, ScVal, ScVec,
-        WriteXdr,
+        ContractDataEntry, Hash, LedgerEntry, LedgerEntryData, LedgerKey, ReadXdr, ScObject,
+        ScStatic, ScVal, ScVec, WriteXdr,
     },
     Host, HostError, Vm,
 };
 
-/// Deserialize an SCVec XDR object of SCVal arguments from the C++ side of the
-/// bridge, instantiate a Host and VM with the provided WASM, invoke the
-/// requested function in the WASM, and serialize an SCVal back into a return
-/// value.
-pub(crate) fn invoke_contract(
-    contract_id: &XDRBuf,
-    func: &CxxString,
-    args: &XDRBuf,
-    footprint: &XDRBuf,
-    ledger_entries: &Vec<XDRBuf>,
-) -> Result<Vec<u8>, Box<dyn Error>> {
-    let contract_id = xdr::Hash::read_xdr(&mut Cursor::new(contract_id.data.as_slice()))?;
-    let arg_scvals = ScVec::read_xdr(&mut Cursor::new(args.data.as_slice()))?;
+/// Helper for [`build_storage_footprint_from_xdr`] that inserts a copy of some
+/// [`AccessType`] `ty` into a [`storage::Footprint`] for every [`LedgerKey`] in
+/// `keys`.
+fn populate_access_map_with_contract_data_keys(
+    access: &mut OrdMap<Key, AccessType>,
+    keys: Vec<LedgerKey>,
+    ty: AccessType,
+) -> Result<(), HostError> {
+    for lk in keys {
+        match lk {
+            LedgerKey::ContractData(xdr::LedgerKeyContractData { contract_id, key }) => {
+                let sk = Key { contract_id, key };
+                access.insert(sk, ty.clone());
+            }
+            _ => return Err(HostError::General("unexpected ledger key type").into()),
+        }
+    }
+    Ok(())
+}
+
+/// Deserializes an [`xdr::Footprint`] from the provided [`XDRBuf`], converts
+/// its entries to an [`OrdMap`], and returns a [`storage::Footprint`]
+/// containing that map.
+fn build_storage_footprint_from_xdr(footprint: &XDRBuf) -> Result<storage::Footprint, HostError> {
     let xdr::Footprint {
         read_only,
         read_write,
     } = xdr::Footprint::read_xdr(&mut Cursor::new(footprint.data.as_slice()))?;
-    let mut map = OrdMap::new();
     let mut access = OrdMap::new();
-    for lk in read_only.to_vec() {
-        match lk {
-            xdr::LedgerKey::ContractData(xdr::LedgerKeyContractData { contract_id, key }) => {
-                let sk = storage::Key { contract_id, key };
-                access.insert(sk, storage::AccessType::ReadOnly);
-            }
-            _ => return Err(HostError::General("unexpected ledger key type").into()),
-        }
-    }
-    for lk in read_write.to_vec() {
-        match lk {
-            xdr::LedgerKey::ContractData(xdr::LedgerKeyContractData { contract_id, key }) => {
-                let sk = storage::Key { contract_id, key };
-                access.insert(sk, storage::AccessType::ReadWrite);
-            }
-            _ => return Err(HostError::General("unexpected ledger key type").into()),
-        }
-    }
+
+    populate_access_map_with_contract_data_keys(
+        &mut access,
+        read_only.to_vec(),
+        AccessType::ReadOnly,
+    )?;
+    populate_access_map_with_contract_data_keys(
+        &mut access,
+        read_write.to_vec(),
+        AccessType::ReadWrite,
+    )?;
+    Ok(storage::Footprint(access))
+}
+
+/// Deserializes a sequence of [`xdr::LedgerEntry`] structures from a vector of
+/// [`XDRBuf`] buffers, inserts them into an [`OrdMap`] with entries
+/// additionally keyed by the provided contract ID, checks that the entries
+/// match the provided [`storage::Footprint`], and returns the constructed map.
+fn build_storage_map_from_xdr_ledger_entries(
+    footprint: &storage::Footprint,
+    ledger_entries: &Vec<XDRBuf>,
+) -> Result<OrdMap<Key, Option<ScVal>>, HostError> {
+    let mut map = OrdMap::new();
     for buf in ledger_entries {
         let le = LedgerEntry::read_xdr(&mut Cursor::new(buf.data.as_slice()))?;
         match le.data {
@@ -63,8 +79,8 @@ pub(crate) fn invoke_contract(
                 val,
                 contract_id,
             }) => {
-                let sk = storage::Key { contract_id, key };
-                if !access.contains_key(&sk) {
+                let sk = Key { contract_id, key };
+                if !footprint.0.contains_key(&sk) {
                     return Err(HostError::General("ledger entry not found in footprint").into());
                 }
                 map.insert(sk.clone(), Some(val));
@@ -72,12 +88,22 @@ pub(crate) fn invoke_contract(
             _ => return Err(HostError::General("unexpected ledger entry type").into()),
         }
     }
-    for k in access.keys() {
+    for k in footprint.0.keys() {
         if !map.contains_key(k) {
             return Err(HostError::General("ledger entry not found for footprint entry").into());
         }
     }
-    let wasm_key = storage::Key {
+    Ok(map)
+}
+
+/// Looks up an [`ScObject`] un the provided map under the ledger key
+/// [`ScStatic::LedgerKeyContractCodeWasm`] and returns a copy of its binary
+/// data, which should be WASM bytecode.
+fn extract_contract_wasm_from_storage_map(
+    contract_id: &Hash,
+    map: &OrdMap<Key, Option<ScVal>>,
+) -> Result<Vec<u8>, HostError> {
+    let wasm_key = Key {
         contract_id: contract_id.clone(),
         key: ScVal::Static(ScStatic::LedgerKeyContractCodeWasm),
     };
@@ -95,11 +121,33 @@ pub(crate) fn invoke_contract(
             )
         }
     };
+    Ok(wasm.to_vec())
+}
+
+/// Deserializes an [`xdr::Hash`] contract ID, an [`ScVec`] XDR object of
+/// arguments, an [`xdr::Footprint`] and a sequence of [`xdr::LedgerEntry`]
+/// entries containing all the data the contract intends to read. Then loads
+/// some WASM bytecode out of the provided ledger entries (keyed under
+/// [`ScStatic::LedgerKeyContractCodeWasm`]), instantiates a [`Host`] and [`Vm`]
+/// with the provided WASM, invokes the requested function in the WASM, and
+/// serializes an [`xdr::ScVal`] back into a return value.
+pub(crate) fn invoke_contract(
+    contract_id: &XDRBuf,
+    func: &CxxString,
+    args: &XDRBuf,
+    footprint: &XDRBuf,
+    ledger_entries: &Vec<XDRBuf>,
+) -> Result<Vec<u8>, Box<dyn Error>> {
+    let contract_id = Hash::read_xdr(&mut Cursor::new(contract_id.data.as_slice()))?;
+    let arg_scvals = ScVec::read_xdr(&mut Cursor::new(args.data.as_slice()))?;
+
+    let footprint = build_storage_footprint_from_xdr(footprint)?;
+    let map = build_storage_map_from_xdr_ledger_entries(&footprint, ledger_entries)?;
+    let wasm = extract_contract_wasm_from_storage_map(&contract_id, &map)?;
 
     let func_str = func.to_str()?;
 
-    let footprint = storage::Footprint(access);
-    let storage = storage::Storage::with_enforcing_footprint_and_map(footprint, map);
+    let storage = Storage::with_enforcing_footprint_and_map(footprint, map);
     let mut host = Host::with_storage(storage);
     let vm = Vm::new(&host, contract_id, wasm.as_slice())?;
 
