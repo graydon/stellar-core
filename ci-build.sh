bee128329fdc2ecae37fffee710701b71442077d
Graydon Hoare <graydon@pobox.com>
2025-09-19 03:01:53 -0700

    ln -sf

--- ci-build.sh
+++ ci-build.sh
@@ -83,19 +83,19 @@ if test $CXX = 'clang++'; then
     # Use CLANG_VERSION environment variable if set, otherwise default to 12
     CLANG_VER=${CLANG_VERSION:-12}
     which clang-${CLANG_VER}
-    ln -s `which clang-${CLANG_VER}` bin/clang
+    ln -sf `which clang-${CLANG_VER}` bin/clang
     which clang++-${CLANG_VER}
-    ln -s `which clang++-${CLANG_VER}` bin/clang++
+    ln -sf `which clang++-${CLANG_VER}` bin/clang++
     which llvm-symbolizer-${CLANG_VER}
-    ln -s `which llvm-symbolizer-${CLANG_VER}` bin/llvm-symbolizer
+    ln -sf `which llvm-symbolizer-${CLANG_VER}` bin/llvm-symbolizer
     clang -v
     llvm-symbolizer --version || true
 elif test $CXX = 'g++'; then
     RUN_PARTITIONS=$(seq $NPROCS $((2*NPROCS-1)))
     which gcc-10
-    ln -s `which gcc-10` bin/gcc
+    ln -sf `which gcc-10` bin/gcc
     which g++-10
-    ln -s `which g++-10` bin/g++
+    ln -sf `which g++-10` bin/g++
     which g++
     g++ -v
 fi
